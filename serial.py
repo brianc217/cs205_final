aa14690a2d827677ff2badf8379d007e50821a28
Brian Connolly <brianc217@gmail.com>
2013-12-08 12:50:26 -0800

    some plotting

--- serial.py
+++ serial.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+import csv
 
 # Global variables, can be used by any process
 dt = 0.1
@@ -36,19 +37,19 @@ class Map:
     else:
       self.robots[robotId].position = (self.robots[robotId].position[0]+dt*self.robots[robotId].velocity[0],self.robots[robotId].position[1]+dt*self.robots[robotId].velocity[1])
       #min x
-      if self.robots[robotId].position[0]<0:
+      if self.robots[robotId].position[0] < 0:
         self.robots[robotId].position = (0,self.robots[robotId].position[1])
         self.robots[robotId].velocity = (0,self.robots[robotId].velocity[1])
       #max x
-      if self.robots[robotId].position[0]>self.width:
+      if self.robots[robotId].position[0] > self.width:
         self.robots[robotId].position = (self.width,self.robots[robotId].position[1])
         self.robots[robotId].velocity = (0,self.robots[robotId].velocity[1])
       #min y
-      if self.robots[robotId].position[1]<0:
+      if self.robots[robotId].position[1] < 0:
         self.robots[robotId].position = (self.robots[robotId].position[0],0)
         self.robots[robotId].velocity = (self.robots[robotId].velocity[0],0)
       #max y
-      if self.robots[robotId].position[1]>self.height:
+      if self.robots[robotId].position[1] > self.height:
         self.robots[robotId].position = (self.robots[robotId].position[0],self.height)
         self.robots[robotId].velocity = (self.robots[robotId].velocity[0],0)
       
@@ -65,7 +66,8 @@ class Robot:
     self.velocity = velocity
     self.finished = False
     self.id = robotId
-
+    self.posHistory = [position]
+    self.velHistory = [velocity]
 
 class Obstacle:
   def __init__(self, function):
@@ -79,14 +81,14 @@ class CircleObstacle(Obstacle):
       return (position[0]-center[0])**2 + (position[1]-center[1])**2 <= radius**2
 
     def moderateSlow(robot):
-      return slowDown(0.1, robot)
+      return slowDown(0.01, robot)
 
     self.contains = circle
     self.updateVelocity = moderateSlow
 
 def slowDown(slowfactor, robot, timespan=1):
   velocity = robot.velocity
-  robot.velocity = (velocity[0](1-slowfactor) * dt / timespan, velocity[1](1-slowfactor) * dt / timespan)
+  robot.velocity = (velocity[0]*(1-slowfactor) ** (dt / timespan), velocity[1]*(1-slowfactor) ** (dt / timespan))
 
 
 if __name__ == '__main__':
@@ -100,15 +102,32 @@ if __name__ == '__main__':
   robots = []
   robots.append(Robot(startPos, startVel, 0))
 
-  globalMap = Map((500, 200), (450,100), 200, obstacles, robots)
+  globalMap = Map((500, 200), (450,100), 555, obstacles, robots)
   
   while time < globalMap.endTime:
-    print "IN LOOP"
     
     for robot in globalMap.robots:
       globalMap.updatePosition(robot.id)
-      print robot.position
+      globalMap.updateVelocity(robot.id)
+      robot.posHistory.append(robot.position)
+      robot.velHistory.append(robot.velocity)
+
     time += dt
+
+  x = []
+  y = []
+
+  for robot in globalMap.robots:
+    for pos in robot.posHistory:
+      x.append(pos[0])
+      y.append(pos[1])
+
+  plt.plot(x,y)
+    
+  '''
+  with open('out.csv', 'wb') as csvfile:
+    writer = csv.writer(csvfile)
+  '''
 
 
 
